43ddd54216f26e0eb8fbb4c24554333d4451099a
il.ya <saldin.iliya@gmail.com>
2015-05-02 11:35:36 +0600

    removed unused VCs; added SearchAndChooseUserTableViewController class and view

--- klngrd_tj/SearchAndChooseUserTableViewController.h
+++ klngrd_tj/SearchAndChooseUserTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SearchAndChooseUserTableViewController.h
 //  klngrd_tj
 //
 //  Created by il.ya sald.in on 5/2/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SearchAndChooseUserTableViewController : UITableViewController
 
 @end
-
